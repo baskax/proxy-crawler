be8237b8625e69b91066f12e9ea87d15b0dd655c
blaszakf <blaszakf@52daa76f-87f5-40fe-bf62-342dbbbd5c91>
2016-04-21 16:48:25 +0000

    spoko

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import connector
 import urllib2
+import time
 from lxml import html
 
 def showIntro():
@@ -24,21 +25,36 @@ ports = tree.xpath('//td[2]/text()')
 values = zip(hosts,ports)
 for h, p in values:
     try:
-        conn.add_proxy(h,p)
-        print((h,p), " added to list")
+        conn.add_proxy(h,p)        
     except:
         continue
+print("Got proxies, checking.")
 proxies = conn.get_proxies_to_work()
+f=0
+s=0
+t=0
 for key in proxies:
     addr = "https://" + key[1] + ":" + str(key[2])
-    proxy =  urllib2.ProxyHandler({'http': addr})
+    proxy =  urllib2.ProxyHandler({'https': addr})
     opener = urllib2.build_opener(proxy)
     urllib2.install_opener(opener)
     try:
-        urllib2.urlopen('http://mojepanstwo.pl/neocraft')
-        conn.update_status(str(key[0]),1)
-	print(addr," sukces")
-    except:        
-        conn.update_status(str(key[0]),2)
-	print(addr, " fail")
-	continue
+	a = urllib2.urlopen("https://prod.ceidg.gov.pl/CEIDG/CEIDG.Public.UI/Search.aspx",timeout=5)
+        start = time.time()	
+    	if a.getcode()==200:
+	    page = a.read()
+            ping=(time.time()-start)*1000
+	    conn.update_status(str(key[0]),1)
+	    conn.update_timeout(str(key[0]),ping)
+	    s+=1       	
+        else:        
+            conn.update_status(str(key[0]),2)
+    	    f+=1
+	    continue
+    except:
+	t+=1
+	conn.update_status(str(key[0]),2)	
+	continue	
+print "Passed - ",s
+print "Failed - ",f
+print "Timeout error - ",t
