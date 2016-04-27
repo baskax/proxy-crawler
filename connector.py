abcb53b208dc0896a3d2500b5124d620566b19f8
blaszakf <blaszakf@52daa76f-87f5-40fe-bf62-342dbbbd5c91>
2016-04-27 09:13:27 +0000

    TO DZIALA

--- connector.py
+++ connector.py
@@ -1,30 +1,28 @@
-import MySQLdb
+import PySQLPool
 
 class Connector:
     
-    __db = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="howtosaygoodbye'x",db="proxy")
-    __cur = __db.cursor()
+    __db = PySQLPool.getNewConnection(host="127.0.0.1",port=3306,user="root",password="howtosaygoodbye'x",db="proxy")
+    __query = PySQLPool.getNewQuery(__db)
     
     def get_proxy(self,id):
-        self.__cur.execute("select host,port,timeout from proxy_list where id=%s" % id)
-        return self.__cur.fetchall()
+        self.__query.Query("select host,port,timeout from proxy_list where id='%s'" % id)
+        return self.__query.record
     
     def get_proxies_to_work(self):
-        self.__cur.execute("select * from proxy_list where status = 0")
-        return self.__cur.fetchall()
+        self.__query.Query("select * from proxy_list where status = 0")
+        return self.__query.record
         
     def get_all_proxies(self):
-        self.__cur.execute("select * from proxy_list")
-        return self.__cur.fetchall()
+        self.__query.Query("select * from proxy_list")
+        return self.__query.record
     
     def update_timeout(self,id,timeout):
-        self.__cur.execute("update proxy_list set timeout='%s' where id=%s;", (timeout,id))
-        self.__db.commit()        
+        self.__query.Query("update proxy_list set timeout='%s' where id=%s", (timeout,id))        
     
     def update_status(self,id,status):
-        self.__cur.execute("update proxy_list set status='%s' where id=%s;", (status,id))
-        self.__db.commit()
+        self.__query.Query("update proxy_list set status='%s' where id=%s", (status,id))
         
     def add_proxy(self,host,port):
-        self.__cur.execute("insert into proxy_list (host,port) values (%s,%s)", (host,port))
-        self.__db.commit()
+        self.__query.Query("insert into proxy_list (host,port) values ('%s','%s')", (host,port))
+            
