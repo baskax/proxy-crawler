ab12e1f5c5ab7e57b1f7c2d2bd5ccb152aa71770
blaszakf <blaszakf@52daa76f-87f5-40fe-bf62-342dbbbd5c91>
2016-04-27 09:13:27 +0000

    TO DZIALA

--- proxy-crawler/main.py
+++ proxy-crawler/main.py
@@ -1,60 +1,94 @@
 import connector
 import urllib2
 import time
+import threading
+import Queue
 from lxml import html
 
-def showIntro():
-    print('#####################################')
-    print('## P R O X Y L I S T C R A W L E R ##')
-    print('#####################################')
-
-
-showIntro()
 conn = connector.Connector()
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-try:
-    proxyList = opener.open("http://www.sslproxies.org/")
-except:
-    print('Failed to open URL, exiting...')
-    exit()
-plain_html = proxyList.read()
-tree = html.fromstring(plain_html)
-hosts = tree.xpath('//td[1]/text()')
-ports = tree.xpath('//td[2]/text()')
-values = zip(hosts,ports)
-for h, p in values:
+
+def crawlProxies(address):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     try:
-        conn.add_proxy(h,p)        
+        proxyList = opener.open(address)
+        print("Opened " + address)
     except:
-        continue
-print("Got proxies, checking.")
-proxies = conn.get_proxies_to_work()
+        print('Failed to open URL, exiting...')
+        exit()
+    plain_html = proxyList.read()
+    tree = html.fromstring(plain_html)
+    hosts = tree.xpath('//td[1]/text()')
+    ports = tree.xpath('//td[2]/text()')
+    values = zip(hosts,ports)
+    for h, p in values:
+        try:
+            conn.add_proxy(h,p)
+        except:
+            continue
+
+crawlProxies("http://www.us-proxy.org/")
+crawlProxies("http://www.sslproxies.org/")
+crawlProxies("http://free-proxy-list.net/uk-proxy.html")
+
 f=0
 s=0
 t=0
-for key in proxies:
-    addr = "https://" + key[1] + ":" + str(key[2])
-    proxy =  urllib2.ProxyHandler({'https': addr})
+
+def validateProxy(key,tConn):
+    global f,s,t    
+    addr = "http://" + key['host'] + ":" + str(key['port'])
+    proxy =  urllib2.ProxyHandler({'http': addr})
     opener = urllib2.build_opener(proxy)
     urllib2.install_opener(opener)
     try:
-	a = urllib2.urlopen("https://prod.ceidg.gov.pl/CEIDG/CEIDG.Public.UI/Search.aspx",timeout=5)
+        a = urllib2.urlopen("http://panoramafirm.pl/",timeout=5)
         start = time.time()	
-    	if a.getcode()==200:
-	    page = a.read()
+        if a.getcode()==200:
+            print "GIT!"
+            page = a.read()
             ping=(time.time()-start)*1000
-	    conn.update_status(str(key[0]),1)
-	    conn.update_timeout(str(key[0]),ping)
-	    s+=1       	
-        else:        
-            conn.update_status(str(key[0]),2)
-    	    f+=1
-	    continue
+            tConn.update_status(str(key['id']),1)
+            tConn.update_timeout(str(key['id']),str(ping))
+            s+=1       	
+        else:
+            tConn.update_status(str(key['id']),2)
+            f+=1            
     except:
-	t+=1
-	conn.update_status(str(key[0]),2)	
-	continue	
+        t+=1
+        tConn.update_status(str(key['id']),2)        
+
+try:    
+    proxies = conn.get_proxies_to_work()
+    print("Got proxies to work")
+except:
+    print("There is no proxy with proper status")
+    exit()
+
+class myThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        tConn = connector.Connector()
+        validateProxy(key,tConn)
+        threads.remove(self)
+
+threads=[]
+
+i = 0
+for key in proxies:
+    i+=1
+    if (i % 50 == 0):
+        print "Done " + i + " proxies"
+    while (len(threads) > 9):    
+        try:
+            myThread.sleep(1000)
+        except:
+            pass
+    thread = myThread()
+    threads.append(thread)
+    thread.start()
+
 print "Passed - ",s
 print "Failed - ",f
 print "Timeout error - ",t
